7902f9161e867d0b728c22a31b096807d5305db9
briancaldass <briansteve-1992@hotmail.com>
2016-09-19 23:30:01 -0500

    Modificacion noticias o entradas de la web

--- index.php
+++ index.php
@@ -13,30 +13,35 @@
     <img src="<?php bloginfo('template_url')?>/img/bannercontexto.png" class="bannercontexto">
 
       <!-- Example row of columns -->
-      <div class="row noticias">
+          <div class="row noticias">
         <div class=" row borde-noticias">
           <h4>Ultimas Noticias</h4>
+          <?php 
+          $args=array(
+            'showposts'=> 3,
+          );
 
+          $my_query = new WP_Query($args)
+          ?>
+
+          <?php if ($my_query->have_posts() ) : while ( $my_query->have_posts() ) :$my_query-> the_post(); ?>
+       
           <div class="col-md-4">
-            <h2>HeadingHeadingHeadadingHeadadingHead</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default boton" href="#" role="button">Ver mas &raquo;</a></p>
+            <h2><?php the_title();?></h2>
+            <p><?php the_excerpt();?></p>
+            <p><a class="btn btn-default boton" href="<?php the_permalink();?>" role="button">Ver mas &raquo;</a></p>
           </div>
-
-          <div class="col-md-4">
-            <h2>HeadingHeadadingHeHeadadingHe</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default boton" href="#" role="button">Ver mas &raquo;</a></p>
-         </div>
-
-          <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default boton" href="#" role="button">Ver mas &raquo;</a></p>
-          </div>
+       
+          <?php endwhile; else: ?>
+           
+              <h2>No hay noticias para mostrar</h2>
+           
+          <?php endif; ?>
 
         </div>
       </div>
+      
+  
 
        
 
